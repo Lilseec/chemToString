46c1bd7e9ac55a181ba7b7c6ba9fbda8430d3418
unknown <yura.rossol@gmail.com>
2021-12-11 00:41:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let updatedElem = document.getElementById('string')
+let string = ''
+
+let period = document.getElementById('PeriodicSysTable')
+period.addEventListener("click", (event) => {
+    let domElement = event.target
+    if (!domElement.className.includes('chem-element')) {
+        domElement = domElement.parentNode
+    } 
+    let element = domElement.getElementsByClassName('id')[0].textContent
+    string += element
+    updatedElem.textContent = string
+})
