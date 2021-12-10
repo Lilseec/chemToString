4a6fd106395b27268375902b95a71cf5197718fd
Lilseec <44337171+Lilseec@users.noreply.github.com>
2021-12-11 00:50:50 +0200

    fixed bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let string = ''
 let period = document.getElementById('PeriodicSysTable')
 period.addEventListener("click", (event) => {
     let domElement = event.target
-    if (!domElement.className.includes('chem-element')) {
+    if (!domElement.className.includes('chem-element') && domElement.className) {
         domElement = domElement.parentNode
     } 
     let element = domElement.getElementsByClassName('id')[0].textContent
